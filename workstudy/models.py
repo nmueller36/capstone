efb7c20e19e6f53944bbea25e3304ece56b8fd32
Nicole Mueller <nmueller@arcadia.edu>
2019-02-13 17:36:46 -0500

    Add SiteInfo model

--- workstudy/models.py
+++ workstudy/models.py
@@ -6,28 +6,13 @@ class PersonalInfo(models.Model):
 	first_name = models.CharField(max_length=256)
 	last_name  = models.CharField(max_length=256)
 	email      = models.CharField(max_length=256)
-	
+
 class AppData(models.Model):
 	personal_info = models.ForeignKey(PersonalInfo, on_delete=models.CASCADE, blank=True, null=True)
 	semester      = models.CharField(max_length=100)
-	
 
-class AppAvailability(models.Model):
-	app_data   = models.ForeignKey(AppData, on_delete=models.CASCADE, blank=True, null=True)
-	day        = models.CharField(max_length=100)
-	start_time = models.IntegerField()
-	end_time   = models.IntegerField()
-	
-class StudentPlacement (models.Model):
-	personal_info = models.ForeignKey(PersonalInfo, on_delete=models.CASCADE, blank=True, null=True)
-	semester      = models.CharField(max_length=100)
-	#site_info     = models.ForeignKey(SiteInfo, on_delete=models.CASCADE, blank=True, null=True)
-	
-class StudentSchedule (models.Model):
-	personal_info = models.ForeignKey(PersonalInfo, on_delete=models.CASCADE, blank=True, null=True)
-	semester      = models.CharField(max_length=100)
 
-class SiteInfo (models.Model):
+class SiteInfo(models.Model):
 	site_name   = models.CharField(max_length=256)
 	address     = models.CharField(max_length=256)
 	description = models.CharField(max_length=1000)
@@ -36,12 +21,26 @@ class SiteInfo (models.Model):
 	supervisor_phone = models.CharField(max_length=256)
 	second_contact = models.CharField(max_length=256)
 	second_contact_email = models.CharField(max_length=256)
-	
-	
-class SiteAvailability (models.Model):
-	#site_info  = models.ForeignKey(SiteInfo, on_delete=models.CASCADE, blank=True, null=True)
-	day        = models.CharField(max_length=100) 
+
+class AppAvailability(models.Model):
+	app_data   = models.ForeignKey(AppData, on_delete=models.CASCADE, blank=True, null=True)
+	day        = models.CharField(max_length=100)
 	start_time = models.IntegerField()
 	end_time   = models.IntegerField()
 
+class StudentPlacement(models.Model):
+	personal_info = models.ForeignKey(PersonalInfo, on_delete=models.CASCADE, blank=True, null=True)
+	site_info     = models.ForeignKey(SiteInfo, on_delete=models.CASCADE, blank=True, null=True)
+	semester      = models.CharField(max_length=100)
 
+class StudentSchedule(models.Model):
+	personal_info = models.ForeignKey(PersonalInfo, on_delete=models.CASCADE, blank=True, null=True)
+	semester      = models.CharField(max_length=100)
+
+
+
+class SiteAvailability(models.Model):
+	site_info  = models.ForeignKey(SiteInfo, on_delete=models.CASCADE, blank=True, null=True)
+	day        = models.CharField(max_length=100)
+	start_time = models.IntegerField()
+	end_time   = models.IntegerField()
